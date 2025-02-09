4a70c87e08f52140a371ac456c967eb5dd557352
luizffdemoraes <lffm1994@gmail.com>
2025-02-08 22:43:00 -0300

    Pacotes e módulos parte 3.

--- 1-Fundacao/22/main.go
+++ 1-Fundacao/22/main.go
@@ -6,15 +6,20 @@ import (
 	"github.com/fullcycle/curso-go/matematica"
 )
 
-/***
+/*
+**
 O comando abaixo cria um arquivo go.mod que é o arquivo de configuração do módulo go
 
 go mod init github.com/fullcycle/curso-go
 
-***/
+# Tudo que começa com letra maiúscula é público e pode ser acessado por outros pacotes
+
+**
+*/
 func main() {
-
-	soma := matematica.Soma(10, 20)
-
+	soma := matematica.Soma(matematica.A, 20)
+	carro := matematica.Carro{Marca: "Fiat"}
 	fmt.Println("Resultado: %v", soma)
+	fmt.Printf("Carro: %v\n", carro)
+	fmt.Println(carro.Andar())
 }
