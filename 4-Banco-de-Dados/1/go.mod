b6a17be33510d026f749dce9ac2f2471858ee2bf
luizffdemoraes <lffm1994@gmail.com>
2025-02-18 00:41:24 -0300

    Inserindo dados no banco.

--- 4-Banco-de-Dados/1/go.mod
+++ 4-Banco-de-Dados/1/go.mod
@@ -1,5 +1,10 @@
-module github.com/devfullcycle/goexpert/6/1
+module goexpert-db
 
 go 1.23.5
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/google/uuid v1.6.0
+)
+
+require filippo.io/edwards25519 v1.1.0 // indirect
--- 4-Banco-de-Dados/1/main.go
+++ 4-Banco-de-Dados/1/main.go
@@ -1,10 +1,15 @@
 package main
 
-import "github.com/google/uuid"
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
 
 /***
 1) - docker-compose up -d
-2) - docker-compose exec mysql bash
+2) - docker-compose exec mysql bash || docker exec -it mysql bash || docker-compose down && docker-compose up -d || docker logs mysql
 3) - mysql -u root -p goexpert
 4) - create table products (id varchar(255), name varchar(80), price decimal(10,2), primary key (id));
 ***/
@@ -25,4 +30,26 @@ func NewProduct(name string, price float64) *Product {
 
 func main() {
 
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	product := NewProduct("Notebook", 1899.90)
+	err = insertProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func insertProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("INSERT INTO products (id, name, price) VALUES (?, ?, ?)") // $1 $2 $3 caso utilize sqlite
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	// result ele contempla os dados que foram inseridos
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	return err
 }
