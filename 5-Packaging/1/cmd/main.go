12605d061736131b933798e4fa14fa847bfc7669
luizffdemoraes <lffm1994@gmail.com>
2025-02-22 20:39:23 -0300

    Acessando pacotes criados

--- 5-Packaging/1/cmd/main.go
+++ 5-Packaging/1/cmd/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/fullcycle/curso-go/5-Packaging/1/math"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	m := math.Math{A: 1, B: 2}
+	fmt.Println(m.Add())
+
+	// fmt.Println("Hello, World!")
 }
