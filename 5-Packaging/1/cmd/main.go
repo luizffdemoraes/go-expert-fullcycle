6730db39f66a9bdc822606c8ce9e44fbf0cdca87
luizffdemoraes <lffm1994@gmail.com>
2025-02-22 22:03:45 -0300

    Exportação de objetos.

--- 5-Packaging/1/cmd/main.go
+++ 5-Packaging/1/cmd/main.go
@@ -7,8 +7,11 @@ import (
 )
 
 func main() {
-	m := math.Math{A: 1, B: 2}
+	m := math.NewMath(1, 2)
+	// m := math.Math{a: 1, b: 2}
+	fmt.Println(m)
 	fmt.Println(m.Add())
+	fmt.Println(math.X)
 
 	// fmt.Println("Hello, World!")
 }
