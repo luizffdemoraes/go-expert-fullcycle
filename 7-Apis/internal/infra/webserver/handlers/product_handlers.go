a39fb65c00d4dedaad415d4e7d10e1506c08dd93
luizffdemoraes <lffm1994@gmail.com>
2025-03-08 00:15:54 -0300

    Buscando e alterando products.

--- 7-Apis/internal/infra/webserver/handlers/product_handlers.go
+++ 7-Apis/internal/infra/webserver/handlers/product_handlers.go
@@ -7,6 +7,8 @@ import (
 	"github.com/fullcycle/curso-go/7-Apis/internal/dto"
 	"github.com/fullcycle/curso-go/7-Apis/internal/entity"
 	"github.com/fullcycle/curso-go/7-Apis/internal/infra/database"
+	entityPkg "github.com/fullcycle/curso-go/7-Apis/pkg/entity"
+	chi "github.com/go-chi/chi/v5"
 )
 
 type ProductHandler struct {
@@ -37,4 +39,53 @@ func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *ProductHandler) GetProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	product, err := h.ProductDB.FindByID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(product)
+}
+
+func (h *ProductHandler) UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	var product entity.Product
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	product.ID, err = entityPkg.ParseID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	_, err = h.ProductDB.FindByID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	err = h.ProductDB.Update(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
