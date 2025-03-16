fb8a4005830b9f65b0d0bcffaded3b0ebfc5ab9a
luizffdemoraes <lffm1994@gmail.com>
2025-03-16 00:27:47 -0300

    Comentários Criando um Load Balancer

--- 8-Multithreading/4-Channels/6-Load-Balancer/main.go
+++ 8-Multithreading/4-Channels/6-Load-Balancer/main.go
@@ -15,20 +15,24 @@ Esse código distribui a carga de trabalho entre múltiplos "workers" concorrent
 **
 */
 func worker(workerId int, data chan int) {
-	for x := range data {
+	for x := range data { // Lê valores do canal até ser fechado
 		fmt.Printf("Worker %d received %d\n", workerId, x)
-		time.Sleep(time.Second)
+		time.Sleep(time.Second) // Simula processamento demorado
 	}
 }
 
 func main() {
+	// Criação de um canal para enviar tarefas aos workers
 	data := make(chan int)
+	// Definição do número de workers que serão criados
 	QtdWorkers := 100000
 
+	// Criação de 100.000 goroutines workers
 	for i := 0; i < QtdWorkers; i++ {
 		go worker(i, data)
 	}
 
+	// Envio de 10.000 tarefas para o canal
 	for i := 0; i < 10000; i++ {
 		data <- i
 	}
